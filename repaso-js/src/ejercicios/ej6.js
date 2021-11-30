85203cad6c55bd4b503f0b15a394d82e1fd08998
Juan <soraioniosif@gmail.com>
2021-11-30 20:45:19 +0100

    Ejercicio 6 finished

--- repaso-js/src/ejercicios/ej6.js
+++ repaso-js/src/ejercicios/ej6.js
@@ -3,10 +3,10 @@ import React from 'react';
 //Añade a "jaime" a mis amigos
 const Ej6 = (props) => {
     const amigos = ["pepe", "paco", "pedro", "luis", "jesus", "jose"];
-
+    amigos.push('Jaime');
     return (
         <div>
-            {/*amigos.???("jaime")*/}
+            {amigos.join(', ')}
         </div>
     )
 }
